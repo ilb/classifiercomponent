af5570e70871d66b09333981305a63b4e3f48dae
ssortia <alexkiyan.lug@gmail.com>
2022-07-29 13:51:07 +0300

    fixes

--- src/classifier/server/utils.mjs
+++ src/classifier/server/utils.mjs
@@ -25,11 +25,7 @@ export const chunkArray = (arr, length) => {
  * @return {*}
  */
 export const prepareClassifies = (classifies, availableClasses) => {
-  return classifies.map(pageClass => {
-    const code = getDossierCode(pageClass);
-
-    return availableClasses.includes(code) ? code : 'otherDocuments';
-  });
+  return classifies.map(code => availableClasses.includes(code) ? code : 'otherDocuments');
 }
 
 export const timeoutPromise = async (promise, err, timeout) => {
@@ -38,46 +34,3 @@ export const timeoutPromise = async (promise, err, timeout) => {
     setTimeout(reject.bind(null, err), timeout * 1000);
   });
 };
-
-function getDossierCode(code) {
-  const codes = {
-    'UNKNOWN': 'unknown',
-    'DOCS_FOR_TREATY_TEMPLATE': 'docsForTreatyTemplate',
-    'BORROWER_QUESTIONNAIRE_TEMPLATE': 'borrowerQuestionnaireTemplate',
-    'LOAN_APPLICATION_TEMPLATE': 'loanApplicationTemplate',
-    'ADD_PRODUCTS_LOAN_TEMPLATE': 'loanApplicationTemplate',
-    'STATEMENTS_TEMPLATE': 'loanApplicationTemplate',
-    'BORROWER_QUESTIONNAIRE_LOAN_APPLICATION_CONSENT_TEMPLATE': 'loanApplicationTemplate',
-    'PASPORT': 'passport',
-    'ANKETA': 'buyerQuestionnaire',
-    'SOGLASIE': 'offerAgreement',
-    'FOTOGRAFIYA': 'buyerPhoto',
-    'PTS': 'vehiclePassport',
-    'VODITELSKOE_UDOSTOVERENIE': 'driverLicense',
-    'SNILS': 'snils',
-    'STS': 'sts',
-    'FOTOGRAFIYA_AUTO': 'vehiclePhoto',
-    'SERTIFIKAT_ZAYAVLENIE_DOP': 'applicationAdditionalProducts',
-    'DOGOVOR_KUPLI-PRODAZHI': 'saleContractAndCheck',
-    'PREV_DOGOVOR_KUPLI-PRODAZHI': 'prevSaleContractAndCheck',
-    'SCHETA_NA_OPLATU': 'invoice',
-    'OPLATA_PERVONACHALNOGO_VZNOSA': 'firstPayment',
-    'DOGOVOR_KOMISSII_AGENTSKIY': 'commissionContract',
-    'SCHETA_NA_OPLATU_SERVICES': 'servicesInvoice',
-    'KASKO': 'kasko',
-    'DOVERENNOST_NA_PRODAZHU_AVTOMOBILYA': 'powerAttorney',
-    'ZAYAVLENIYA_BROKER': 'statementBroker',
-    'KREDITNYY_DOGOVOR': 'loanAgreement',
-    'SERTIFIKAT': 'certificate',
-    'DOGOVOR_MEDITSINSKOGO_STRAKHOVANIYA': 'medicalInsuranceContract',
-    'ZAYAVLENIYE_O_PREDOSTAVLENII_KREDITA': 'loanApplication',
-    'ANKETA_ZAYEMSHCHIKA': 'borrowerQuestionnaire',
-    'AKT_PRIEMA_PEREDACHI_AVTO': 'acceptanceCertificate',
-    'CHECK_LIST_PO_MAYAKU': 'lighthouseChecklist',
-    'FOTO_YASTANOVKI_MAYAKA': 'lighthousePhoto',
-    'OTKAZ_OT_SDACHI_PTS': 'refusalVehiclePassport',
-    'OTHER': 'otherDocuments',
-  }
-
-  return codes[code];
-}
